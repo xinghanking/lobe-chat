31230ddbee333035e78c17ad4aaa828b4478e8d3
vual <25891219+vual@users.noreply.github.com>
2025-03-24 19:23:12 +0800

    🔨 chore: upgrade antd version (#7124)
    Property 'variant' does not exist on type 'IntrinsicAttributes & CardProps & RefAttributes<HTMLDivElement>'.

--- src/const/branding.ts
+++ src/const/branding.ts
@@ -2,12 +2,12 @@
 // if you want to use it in the commercial usage
 // please contact us for more information: hello@lobehub.com
 
-export const LOBE_CHAT_CLOUD = 'LobeChat Cloud';
+export const LOBE_CHAT_CLOUD = 'NewtonAi Cloud';
 
-export const BRANDING_NAME = 'LobeChat';
+export const BRANDING_NAME = 'NewtonAi';
 export const BRANDING_LOGO_URL = '';
 
-export const ORG_NAME = 'LobeHub';
+export const ORG_NAME = 'NewtonAi';
 
 export const BRANDING_URL = {
   help: undefined,
