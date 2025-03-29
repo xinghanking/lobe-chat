aa603d793c3278b3416b5e55386d759ed702f6e5
刘重量 <39993639+xinghanking@users.noreply.github.com>
2025-03-29 18:07:05 +0800

    upgrade

--- src/const/settings/llm.ts
+++ src/const/settings/llm.ts
@@ -14,11 +14,9 @@ export const DEFAULT_LLM_CONFIG = genUserLLMConfig({
   },
 });
 
-export const DEFAULT_MODEL = 'gpt-4o-mini';
+export const DEFAULT_MODEL = 'DeepSeek R1';
 
 export const DEFAULT_EMBEDDING_MODEL = 'text-embedding-3-small';
-export const DEFAULT_EMBEDDING_PROVIDER = ModelProvider.OpenAI;
-
 export const DEFAULT_RERANK_MODEL = 'rerank-english-v3.0';
 export const DEFAULT_RERANK_PROVIDER = 'cohere';
 export const DEFAULT_RERANK_QUERY_MODE = 'full_text';
